d9f8680bde3a0476bcbf17695924c9f0ef0da1de
Trochez <tromon92@gmail.com>
2021-03-13 04:27:54 -0500

    security and availability endpoints

---
+++ src/main/java/com/swagger/swagger/api/shutdown.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swagger.swagger.api;
+
+import com.swagger.swagger.entity.patient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author trocha
+ */
+
+@RestController
+@RequestMapping("/shutdown")
+@Api(value = "Patient microservice")
+public class shutdown implements ApplicationContextAware {
+    
+    private ApplicationContext context;
+    
+    @GetMapping("")
+    @ApiOperation(value = "shutdown patients application", notes = "shutdown patients application")
+    public String shutdown(){
+        
+        ((ConfigurableApplicationContext) context).close();
+        return "Closing application, bye";
+        //System.out.println("in getpatients");
+    }
+    
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.context = ctx;
+        
+    }
+}
